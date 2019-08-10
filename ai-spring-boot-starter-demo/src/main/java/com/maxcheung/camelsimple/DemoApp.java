0edd27fdaa3ae6522dc3a09f591f353dcaa0cf6e
Max Cheung <maxcheung@optusnet.com.au>
2019-08-11 09:28:29 +1000

    Upgrade springboot and camel version

--- ai-spring-boot-starter-demo/src/main/java/com/maxcheung/camelsimple/DemoApp.java
+++ ai-spring-boot-starter-demo/src/main/java/com/maxcheung/camelsimple/DemoApp.java
@@ -4,9 +4,10 @@ import static com.google.common.base.Joiner.on;
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static java.lang.String.format;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.google.common.collect.ImmutableMap;
@@ -32,32 +33,6 @@ public class DemoApp extends SpringBootServletInitializer {
      private static final String DEFAULT_VERSION = "1.0.0";
      private static final String DEFAULT_VENDOR = "Agile Integration";
      private static final String DEFAULT_DESCRIPTION = "Agile Integration Starter Demo App.";
-     
-     /**
-      * {@inheritDoc}
-      */
-     @Override
-     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-         // Obtain information where possible from Manifest file.
-         final String title = firstNonNull(DemoApp.class.getPackage().getImplementationTitle(), DEFAULT_TITLE);
-         final String version = firstNonNull(DemoApp.class.getPackage().getImplementationVersion(), DEFAULT_VERSION);
-         final ImmutableMap.Builder<String, Object> properties = ImmutableMap.<String, Object>builder()
-                 .put("application.title", title)
-                 .put("application.version", version)
-                 .put("application.vendor", DEFAULT_VENDOR)
-                 .put("application.description", DEFAULT_DESCRIPTION);
-                 
-         return builder.sources(DemoApp.class)
-                 .properties(properties.build())
-                 .web(true)
-                 .banner((environment, sourceClass, out) -> {
-                     for (String line : BANNER) {
-                         out.print(format("\n%s%s", INDENTATION, line));
-                     }
-              
-                     out.print(format("\n%s%s : %s (%s)\n\n", INDENTATION, title, version, on(",").join(environment.getActiveProfiles())));
-                 });
-     }
 
      /**
       * Start entry point for the application service.
